97254b66c31104c300c82469600a2e9ef07138c9
alesm97 <32259637+alesm97@users.noreply.github.com>
2018-02-14 13:42:52 +0100

    First update. First scene with a sphere which should change color when looked.

---
+++ SaladilloVR/Assets/Scripts/SphereScript.cs
@@ -0,0 +1,40 @@
+﻿
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereScript : MonoBehaviour
+{
+
+	// Material de la esfera cuando no está siendo mirada
+	public Material sphereMaterial;
+	// Material de la esfera cuando está siendo mirada
+	public Material sphereHoverMaterial;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		GetComponent<Renderer>().material = sphereMaterial;
+	}
+
+	/// <summary>
+	/// Método que se ejecuta cuando se empieza a mirar la esfera.
+	/// </summary>
+	/// <remarks> cambia el material de la esfera al indicado que se debe mostrar cuando se mira la esfera.</remarks>
+	public void HoveredSphere()
+	{
+		GetComponent<Renderer>().material = sphereHoverMaterial;
+	}
+	
+	/// <summary>
+	/// Método que se ejecuta cuando se deja de mirar la esfera.
+	/// </summary>
+	/// <remarks> cambia el material de la esfera al indicado que se debe mostrar cuando se deja de mirar la esfera.</remarks>
+	public void NotHoveredSphere()
+	{
+		GetComponent<Renderer>().material = sphereMaterial;
+	}
+	
+	
+}
